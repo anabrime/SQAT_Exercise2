5087a6939ffdb0f4f5c1267582f73d4117f87b26
somename <someemail>
2016-10-19 14:20:02 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -4,6 +4,9 @@
 // Finish time:
 
 public class PlanetExplorer {
+	private int planetSizeX;
+	private int planetSizeY;
+	
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
@@ -12,6 +15,8 @@ public class PlanetExplorer {
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  
 		 
 	 */
+		this.planetSizeX = x;
+		this.planetSizeY = y;
 	}
 	
 	public PlanetExplorer(int x, int y) {
@@ -34,6 +39,6 @@ public class PlanetExplorer {
 	}
 	
 	public String getPlanetSize() {
-		return "100x100";
+		return this.planetSizeX + "x" + this.planetSizeY;
 	}
 }
