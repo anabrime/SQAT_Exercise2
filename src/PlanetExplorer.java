0d164f24e2909c4ca7ac202c627b4b2275926173
somename <someemail>
2016-10-19 15:23:46 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -48,7 +48,7 @@ public class PlanetExplorer {
 			return;
 		}
 		
-		obstacles = obstacles.replaceFirst("(", "");
+		obstacles = obstacles.replaceFirst("\\(", "");
 		obstacles = obstacles.replaceFirst(")$", "");
 		String[] obstacleCoordinates = obstacles.split(")(");
 		
